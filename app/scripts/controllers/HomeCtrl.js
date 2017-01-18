e858d823fd2f53f7ead765b1680f352f97dda6f7
Chris Holland <chris.holland987@gmail.com>
2017-01-18 10:34:02 -0800

    Checkpoint 4 Messages per Room Completed

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -3,6 +3,15 @@
        this.room = Room;
        this.message = Message;
        this.chatRooms = Room.all;
+       this.currentMessages = [];
+       this.currentRoom = null;
+       this.selectRoom = function(room) {
+           // Set the current room
+           this.currentRoom = room;
+           // filter the current messges
+           this.currentMessages = this.message.getByRoomId(room.$id);
+           
+       };
        console.log(Room.all);
        
        // somehow link form in view to service through here
--- dist/scripts/services/Messages.js
+++ dist/scripts/services/Messages.js
@@ -6,10 +6,23 @@
         
         // Test that messages display
         
+        var createMessage = function(newMessage, assignRoomId, user) {
+            messages.$add({content: newMessage, roomId: assignRoomId, username: user});   
+        };
+        
         return {
             getByRoomId: function(roomId) {
                 // Filter the messages by their room ID
-            }
+                var query = ref.orderByChild("roomId").equalTo(roomId);
+                
+                var messages = $firebaseArray(query);
+                
+                return messages;
+                
+                // test using a specific room - need to change after it is working
+                 
+            },
+             all: messages
         };
     }
         
